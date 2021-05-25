9e6162732e0122f3bad25e80278048d0c422b3d7
BadrBelkadi <Badrdinbelkadi@gmail.com>
2021-05-26 00:15:26 +0100

    Using utility module for a cleaner code

--- Processor.py
+++ Processor.py
@@ -3,69 +3,66 @@ import numpy as np
 import cv2
 from utility import *
 
+if __name__ == '__main__':
+    img = cv2.imread("Image2.png")  # Reading the image
 
-img = cv2.imread("Image2.png")  # Reading the image
+    im = Image.open("Image2.png")  # Opening the image
 
-im = Image.open("Image2.png")  # Opening the image
+    # Converting the image from RGB(colored) into Gray image
 
-# Converting the image from RGB(colored) into Gray image
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    mat = np.array(img)  # Creating an array/matrix from the image
 
-mat = np.array(img)  # Creating an array/matrix from the image
+    # Creating arrays/matrix filled with zeros that have the same size as the given image
 
+    d2y = np.zeros(im.size)
+    d2x = np.zeros(im.size)
+    lap = np.zeros(im.size)
+    l = np.zeros(im.size)
 
-# Creating arrays/matrix filled with zeros that have the same size as the given image
+    # Getting the dimensions of the image
 
-d2y = np.zeros(im.size)
-d2x = np.zeros(im.size)
-lap = np.zeros(im.size)
-l = np.zeros(im.size)
+    [n, m] = im.size
 
+    d2x = derive2x(mat, m)
 
-# Getting the dimensions of the image
+    d2y = derive2y(mat, m)
 
-[n, m] = im.size
+    lap = laplacian(mat, m)
 
+    I = Shadow(mat, m)
 
-d2x = derive2x(mat, m)
+    mar = np.array(img)
 
-d2y = derive2y(mat, m)
+    P = expI(mar, m)
 
-lap = laplacian(mat, m)
+    R = expI(I, m)
 
-I = Shadow(mat, m)
+    R = normalisation(R, np.amax(mar))
 
-mar = np.array(img)
+    P = np.zeros(im.size)
 
-P = expI(mar, m)
+    mar = np.array(img)
 
-R = expI(I, m)
+    l = log(mar, m)
 
-R = normalisation(R, np.amax(mar))
+    textss = text(l, I, m)
 
-P = np.zeros(im.size)
+    textss = expI(textss, m)
 
-mar = np.array(img)
+    textss = normalisation(textss, np.amax(mat))
 
-l = log(mar, m)
+    # Saving the image with Clean text
 
-textss = text(l, I, m)
+    new_im7 = Image.fromarray(textss)
+    if new_im7.mode != 'RGB':
+        new_im7 = new_im7.convert('RGB')
+    new_im7.save("text.png")
 
-textss = expI(textss, m)
+    # Saving the image with shadow only
 
-textss = normalisation(textss, np.amax(mat))
-
-# Saving the image with Clean text
-
-new_im7 = Image.fromarray(textss)
-if new_im7.mode != 'RGB':
-    new_im7 = new_im7.convert('RGB')
-new_im7.save("text.png")
-
-# Saving the image with shadow only
-
-new_im6 = Image.fromarray(R)
-if new_im6.mode != 'RGB':
-    new_im6 = new_im6.convert('RGB')
-new_im6.save("font.png")
+    new_im6 = Image.fromarray(R)
+    if new_im6.mode != 'RGB':
+        new_im6 = new_im6.convert('RGB')
+    new_im6.save("font.png")
